afc0423e33a349780de2bf65e3ad5c5ae3364df0
Atila <atilahunesco@gmail.com>
2025-10-21 21:15:01 -0300

    feat: components and store to the workspace and modals in the main components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Modals } from "@/components/modals";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ConvexClientProvider>
+            <Modals/>
             {children}
           </ConvexClientProvider>
         </body>
---
+++ src/components/modals.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal"
+import { useState } from "react"
+
+export const Modals = ()=> {
+    const [] = useState()
+
+    return (
+        <>
+            <CreateWorkspaceModal/>
+        </>
+    )
+}
