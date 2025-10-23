88195ee0401b24fbb00b0b1ac6bf4ca8c29be358
Atila <atilahunesco@gmail.com>
2025-10-22 21:51:51 -0300

    feat: adding api logic for workspaces.

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,10 +1,16 @@
 "use client"
 
 import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const Modals = ()=> {
-    const [] = useState()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(()=> {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
 
     return (
         <>
--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal"
+import { useCreateWorkspace } from "../api/use-create-workspace"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { 
     Dialog,
     DialogContent,
@@ -7,13 +11,25 @@ import {
     DialogHeader,
     DialogTitle
 } from "@/components/ui/dialog"
-import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal"
 
 export const CreateWorkspaceModal = ()=> {
-    const [open, setOpen] = useCreateWorkspaceModal()
-    
+    const [open, setOpen] = useCreateWorkspaceModal()    
+
+    const { mutate } = useCreateWorkspace()
+
     const handleClose = ()=> {
         setOpen(false)
+    }
+
+    const handleSubmit = async ()=> {
+        const data = await mutate({
+            name: "Workspace 1",
+        }, 
+        {
+            onSuccess(data) {
+
+            }
+        })
     }
 
     return (
@@ -22,6 +38,19 @@ export const CreateWorkspaceModal = ()=> {
                 <DialogHeader>
                     <DialogTitle>Add a workspace</DialogTitle>
                 </DialogHeader>
+                <form className="space-y-4">
+                    <Input
+                    placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                    disabled={false}
+                    minLength={3}
+                    autoFocus
+                    value=""
+                    required/>
+
+                    <div className="flex justify-end">
+                        <Button disabled={false}>Create</Button>
+                    </div>
+                </form>
             </DialogContent>
         </Dialog>
     )
