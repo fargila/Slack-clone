b83957b49aecf254bfed7a2f444f5ff736603ad8
Atila <atilahunesco@gmail.com>
2025-10-24 23:24:16 -0300

    style: adding minimun style to the workspaceId page and layout

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace"
 import { useWorkspaceId } from "@/hooks/use-workspace-id"
 
 const WorkspaceIdPage = ()=> {
     const workspaceId = useWorkspaceId()
-    
+    const { data } = useGetWorkspace({id: workspaceId})
+
     return (
         <div>
-            ID: {workspaceId}
+            Data: {JSON.stringify(data)}
         </div>
     )
 }
---
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+import { Search } from "lucide-react"
+
+export const Toolbar = () => {
+    return (
+        <nav className="bg-purple-700 flex items-center justify-between h-10 p-1.5">
+            <div className="flex-1"></div>
+            <div className="min-w-[280px] max-[642px] grow-[2] shrink">
+                <Button size="sm" 
+                className="bg-accent/25 hover:bg-accent-25 w-full justify-start h-7 px-2">
+                    <Search className="size-4 text-white mr-2"/>
+                    <span className="text-white text-xs">Search workspace</span>
+                </Button>
+            </div>
+        </nav>
+    )   
+}
